88231bab90ab7de9ed967c64fb75319cfc0754d7
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-08-25 10:17:56 -0400

    shell.nix: pin nixpkgs to recent 18.03

--- shell.nix
+++ shell.nix
@@ -1,5 +1,13 @@
 # nix-shell --pure shell.nix --command 'cd src; make'
-{ pkgs ? import <nixpkgs> {} }: with pkgs;
+let
+  rev = "4df3426f5a5e78cef4835897a43abd9e2a092b74";
+  sha256 = "05k5mssiqxffxi45mss9wjns6k76i248rpasa48akdcriry1mp63";
+  nixpkgs = builtins.fetchTarball {
+    inherit sha256;
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/${rev}.tar.gz";
+  };
+in
+{ pkgs ? import nixpkgs {} }: with pkgs;
 
 mkShell {
   FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ inconsolata-lgc libertine libertinus ]; };
