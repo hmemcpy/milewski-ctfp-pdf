e87032f3330623c6c4285464a367fb3c257dc38b
Ahmad Ragab <ragaa820@newschool.edu>
2019-09-05 01:16:06 -0400

    tupeling return type in prodToSum method in section 6.4

--- src/content/1.6/code/scala/snippet34.scala
+++ src/content/1.6/code/scala/snippet34.scala
@@ -1,6 +1,6 @@
 def prodToSum[A, B, C]: ((A, Either[B, C])) => Either[(A, B), (A, C)] = {
   case (x, e) => e match {
-    case Left(y) => Left(x, y)
-    case Right(z) => Right(x, z)
+    case Left(y) => Left((x, y))
+    case Right(z) => Right((x, z))
   }
 }
