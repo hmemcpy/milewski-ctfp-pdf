e237d7d3a541ab139196efcc850025368e257ae4
Joe Kachmar <me@jkachmar.com>
2020-08-02 21:59:14 -0400

    Factors texlive out of mkShell

--- shell.nix
+++ shell.nix
@@ -1,64 +1,77 @@
-{ pkgs ? import ./pinned.nix {} }: with pkgs;
+{ pkgs ? import ./pinned.nix {} }:
 
-mkShell {
-  FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ inconsolata-lgc libertine libertinus]; };
-  buildInputs = [
-    (texlive.combine {
-        inherit (texlive)
-        bookcover
-        textpos
-        fgruler
-        tcolorbox
-        fvextra
-        framed
-        newtx
-        nowidow
-        emptypage
-        wrapfig
-        subfigure
-        adjustbox
-        collectbox
-        tikz-cd
-        imakeidx
-        idxlayout
-        titlesec
-        subfiles
-        lettrine
-        upquote
-        libertine
-        mweights
-        fontaxes
-        mdframed
-        needspace
-        xifthen
-        ifnextok
-        currfile
-        noindentafter
-        ifmtarg
-        scheme-medium
-        listings
-        minted
-        microtype
-        babel
-        todonotes
-        chngcntr
-        ifplatform
-        xstring
-        minifp
-        titlecaps
-        enumitem
-        environ
-        trimspaces
-        l3packages
-        zref
-        catchfile
-        import;
-      })
+let
+  #############################################################################
+  # LaTeX Environment
+  texliveEnv = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      bookcover
+      textpos
+      fgruler
+      tcolorbox
+      fvextra
+      framed
+      newtx
+      nowidow
+      emptypage
+      wrapfig
+      subfigure
+      adjustbox
+      collectbox
+      tikz-cd
+      imakeidx
+      idxlayout
+      titlesec
+      subfiles
+      lettrine
+      upquote
+      libertine
+      mweights
+      fontaxes
+      mdframed
+      needspace
+      xifthen
+      ifnextok
+      currfile
+      noindentafter
+      ifmtarg
+      scheme-medium
+      listings
+      minted
+      microtype
+      babel
+      todonotes
+      chngcntr
+      ifplatform
+      xstring
+      minifp
+      titlecaps
+      enumitem
+      environ
+      trimspaces
+      l3packages
+      zref
+      catchfile
+      import
+      ;
+  };
+in
+
+pkgs.mkShell {
+  FONTCONFIG_FILE = pkgs.makeFontsConf { 
+    fontDirectories = with pkgs; [ inconsolata-lgc libertine libertinus]; 
+  };
+
+  buildInputs = with pkgs; [
+    # Misc. build tooling. 
     gnumake
-    which
     git
     python3Packages.virtualenv
+    which
+    # LaTeX Environment.
+    texliveEnv
   ];
+
   shellHook = ''
     # set SOURCE_DATE_EPOCH so that we can use python wheels
     SOURCE_DATE_EPOCH=$(date +%s)
