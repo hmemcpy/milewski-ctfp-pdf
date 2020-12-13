5e689cc448527cfda84d8efe26f97ae620ad116d
pspencil <3211831+pspencil@users.noreply.github.com>
2020-12-13 18:32:05 +0000

    Fix error in type for bimap
    The `t` I added actually stands for `BiCompBiFunctor.t` but technically `bimap` is defined inside the module so I omitted the module name.

--- src/content/1.8/code/ocaml/snippet13.ml
+++ src/content/1.8/code/ocaml/snippet13.ml
@@ -1,1 +1,1 @@
-val bimap : (a FU.t -> a' FU.t) -> (b GU.t -> b' GU.t) -> (a FU.t, b GU.t) -> (a' FU.t, b' GU.t)
+val bimap : (a FU.t -> a' FU.t) -> (b GU.t -> b' GU.t) -> (a FU.t, b GU.t) t -> (a' FU.t, b' GU.t) t
