0540502a79ab0bc7440c501032ae4226185df595
Georgy Komarov <jubnzv@gmail.com>
2021-11-02 10:42:11 +0300

    Fix formatting in OCaml snippets

--- src/content/1.8/code/ocaml/snippet09.ml
+++ src/content/1.8/code/ocaml/snippet09.ml
@@ -1,4 +1,7 @@
-(** OCaml doesn't support higher kinded types. So, we have to use module functors to emulate the behavior higher kinded types. There's less verbose options using type defunctionalization but it's more advanced and obscures the flow of this book *)
+(** OCaml doesn't support higher kinded types. So, we have to use
+    module functors to emulate the behavior higher kinded types.
+    There's less verbose options using type defunctionalization
+    but it's more advanced and obscures the flow of this book *)
 module type BiComp = functor
   (BF : sig
      type ('a, 'b) t
