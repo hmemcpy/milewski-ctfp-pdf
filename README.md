de799935b221075d526f38345a9b637d4a418782
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-02-02 10:53:45 +0100

    refactor: CI and nix (#306)
    * fix: remove custom fonts
    
    Since they are available in Nix, there is no need to keep them in the project anymore
    
    * chore: remove old obsolete files
    
    * refactor: rewrite Nix files
    
    - Switch from `numtide/flake-utils` to `flake-parts`
    - Add custom font derivation for LaTeX
    - Add `formatter`
    - Switch to `python311`
    
    * ci: update Github workflows
    
    * feat: add `Makefile` for local development
    
    Very useful when used in combination with `nix develop`
    
    * feat: add `.envrc` file for loading development environment with `nix-direnv`
    
    * feat: add `.editorconfig` and `.prettierrc`
    
    * style: reformat files using `prettier`
    
    Run `nix run nixpkgs#nodePackages.prettier -- --write .`
    
    * fix: add workaround to prevent bug with `minted` package
    
    see https://github.com/gpoore/minted/issues/353 for context
    
    * fix: add `version.tex` in the repo
    
    * chore: rewrite `README`
    
    * chore: ignore LaTeX temporary files while building locally
    
    * feat: add `latexindent.pl` configuration file
    
    * style: lint LaTeX files

--- README.md
+++ README.md
@@ -1,76 +1,106 @@
-Category Theory for Programmers
-====
-![image](https://user-images.githubusercontent.com/601206/43392303-f770d7be-93fb-11e8-8db8-b7e915b435ba.png)
-<b>Direct link: [category-theory-for-programmers.pdf](https://github.com/hmemcpy/milewski-ctfp-pdf/releases/download/v1.3.0/category-theory-for-programmers.pdf)</b>  
-(Latest release: v1.3.0, August 2019. See [releases](https://github.com/hmemcpy/milewski-ctfp-pdf/releases) for additional formats and languages.)
+![GitHub stars][github stars]
+[![GitHub Workflow Status][github workflow status]][github actions link]
+[![Download][download badge]][github latest release]
+[![License][license badge]][github latest release]
 
-[![Build Status](https://travis-ci.org/hmemcpy/milewski-ctfp-pdf.svg?branch=master)](https://travis-ci.org/hmemcpy/milewski-ctfp-pdf)  
-[(latest CI build)](https://s3.amazonaws.com/milewski-ctfp-pdf/category-theory-for-programmers.pdf)
+# Category Theory For Programmers
 
-<img src="https://user-images.githubusercontent.com/601206/47271389-8eea0900-d581-11e8-8e81-5b932e336336.png"
- alt="Buy Category Theory for Programmers" width=410 />  
-**[Available in full-color hardcover print](https://www.blurb.com/b/9621951-category-theory-for-programmers-new-edition-hardco)**  
-Publish date: 12 August, 2019. Based off release tag [v1.3.0](https://github.com/hmemcpy/milewski-ctfp-pdf/releases/tag/v1.3.0). See [errata-1.3.0](errata-1.3.0.md) for changes and fixes since print.
+An _unofficial_ PDF version of "**C**ategory **T**heory **F**or **P**rogrammers"
+by [Bartosz Milewski][bartosz github], converted from his [blogpost
+series][blogpost series] (_with permission!_).
 
-**[Scala Edition is now available in paperback](https://www.blurb.com/b/9603882-category-theory-for-programmers-scala-edition-pape)**  
-Publish date: 12 August, 2019. Based off release tag [v1.3.0](https://github.com/hmemcpy/milewski-ctfp-pdf/releases/tag/v1.3.0). See [errata-scala](errata-scala.md) for changes and fixes since print.
+![Category Theory for Programmers][ctfp image]
 
-This is an *unofficial* PDF version of "Category Theory for Programmers" by Bartosz Milewski, converted from his [blogpost series](https://bartoszmilewski.com/2014/10/28/category-theory-for-programmers-the-preface/) (with permission!)
+## Buy the book
 
----
+- **[Standard edition in full-color hardcover
+  print][buy regular edition on blurb]**
+  - Publish date: 12 August, 2019.
+  - Based off release tag [v1.3.0][v1.3.0 github release link]. See
+    [errata-1.3.0](errata-1.3.0.md) for changes and fixes since print.
+- **[Scala Edition in paperback][buy scala edition on blurb]**
+  - Publish date: 12 August, 2019.
+  - Based off release tag [v1.3.0][v1.3.0 github release link]. See
+    [errata-scala](errata-scala.md) for changes and fixes since print.
 
-Building
---------
+## Build the book
 
-The best way to build the book is using the [Nix](https://nixos.org/nix/) package manager. After [installing Nix](https://nixos.org/download.html), if you're using a non-NixOS operating system, you need to install `nixFlakes` in your environment following the steps below ([source](https://nixos.wiki/wiki/Flakes#Non-NixOS)):
+The building workflow requires [Nix][nix website]. After [installing
+Nix][nix download website], you need to enable the upcoming "flake" feature
+which must be [enabled manually][nixos wiki flake] the time being. This is
+needed to expose the new Nix commands and flakes support that are hidden behind
+feature-flags.
 
-```bash
-$ nix-env -iA nixpkgs.nixFlakes
-```
+Afterwards, type `nix flake show` in the root directory of the project to see
+all the available versions of this book. Then type `nix build .#<edition>` to
+build the edition you want (Haskell, Scala, OCaml, Reason and their printed
+versions). For example, to build the Scala edition you'll have to type
+`nix build .#ctfp-scala`.
 
-Edit either `~/.config/nix/nix.conf` or `/etc/nix/nix.conf` and add:
+Upon successful compilation, the PDF file will be placed in the `result`
+directory.
 
-```
-experimental-features = nix-command flakes
-```
+The command `nix develop` will provide a shell containing all the required
+dependencies to build the book manually using the provided `Makefile`. To build
+the `ctfp-scala` edition, just run `make ctfp-scala`.
 
-This is needed to expose the Nix 2.0 CLI and flakes support that are hidden behind feature-flags.
+## Contribute
 
-Also, if the Nix installation is in multi-user mode, don’t forget to restart the nix-daemon. 
+Contributors are welcome to contribute to this book by sending pull-requests.
+Once reviewed, the changes are merged in the main branch and will be
+incorporated in the next release.
 
-Afterwards, type `nix flake show` in the root directory of the project to see all the available versions of this book. Then type `nix build .#<edition>` to build the edition you want (Haskell, Scala, OCaml, Reason and their printed versions). For example, to build the Scala edition you'll have to type `nix build .#ctfp-scala`.
+**Note from [Bartosz][bartosz github]**: I really appreciate all your
+contributions. You made this book much better than I could have imagined. Thank
+you!
 
-Upon successful compilation, the PDF file will be placed in the `result` directory inside the root directory `milewski-ctfp-pdf` of the repository. 
+Find the [list of contributors on Github][contributors].
 
-The file `preamble.tex` contains all the configuration and style declarations.
+## Acknowledgements
 
-Acknowledgements
-----------------
+PDF LaTeX source and the tools to create it are based on the work by [Andres
+Raba][andres raba github]. The book content is taken, with permission, from
+[Bartosz Milewski][bartosz github]'s blogpost series, and adapted to the LaTeX
+format.
 
-PDF LaTeX source and the tools to create it are based on the work by Andres Raba et al., available here: https://github.com/sarabander/sicp-pdf.  
-The book content is taken, with permission, from Bartosz Milewski's blogpost series, and adapted to the LaTeX format.
+The original blog post acknowledgments by Bartosz are consolidated in the
+_Acknowledgments_ page at the end of the book.
 
-Thanks to the following people for contributing corrections/conversions and misc:
+## License
 
-* Oleg Rakitskiy
-* Jared Weakly
-* Paolo G. Giarrusso
-* Adi Shavit
-* Mico Loretan
-* Marcello Seri
-* Erwin Maruli Tua Pakpahan
-* Markus Hauck
-* Yevheniy Zelenskyy
-* Ross Kirsling
-* ...and many others!
+The PDF book, `.tex` files, and associated images and figures in directories
+`src/fig` and `src/content` are licensed under [Creative Commons
+Attribution-ShareAlike 4.0 International License][license cc by sa].
 
-The original blog post acknowledgments by Bartosz are consolidated in the *Acknowledgments* page at the end of the book.
+The script files `scraper.py` and others are licensed under [GNU General Public
+License version 3][license gnu gpl].
 
-**Note from Bartosz**: I really appreciate all your contributions. You made this book much better than I could have imagined. Thank you!
-
-License
--------
-
-The PDF book, `.tex` files, and associated images and figures in directories `src/fig` and `src/content` are licensed under Creative Commons Attribution-ShareAlike 4.0 International License ([cc by-sa](http://creativecommons.org/licenses/by-sa/4.0/)).
-
-The script files `scraper.py` and others are licensed under GNU General Public License version 3 (for details, see [LICENSE](https://github.com/hmemcpy/milewski-ctfp-pdf/blob/master/LICENSE)).
+[download badge]:
+  https://img.shields.io/badge/Download-latest-green.svg?style=flat-square
+[github actions link]: https://github.com/hmemcpy/milewski-ctfp-pdf/actions
+[github stars]:
+  https://img.shields.io/github/stars/hmemcpy/milewski-ctfp-pdf.svg?style=flat-square
+[github workflow status]:
+  https://img.shields.io/github/actions/workflow/status/hmemcpy/milewski-ctfp-pdf/build.yml?branch=master&style=flat-square
+[github latest release]:
+  https://github.com/hmemcpy/milewski-ctfp-pdf/releases/latest
+[license badge]:
+  https://img.shields.io/badge/License-CC_By_SA-green.svg?style=flat-square
+[ctfp image]:
+  https://user-images.githubusercontent.com/601206/47271389-8eea0900-d581-11e8-8e81-5b932e336336.png
+[bartosz github]: https://github.com/BartoszMilewski
+[nixos wiki flake]: https://nixos.wiki/wiki/Flakes
+[andres raba github]: https://github.com/sarabander
+[contributors]: https://github.com/hmemcpy/milewski-ctfp-pdf/graphs/contributors
+[license cc by sa]: https://spdx.org/licenses/CC-BY-SA-4.0.html
+[license gnu gpl]: https://spdx.org/licenses/GPL-3.0.html
+[blogpost series]:
+  https://bartoszmilewski.com/2014/10/28/category-theory-for-programmers-the-preface/
+[buy regular edition on blurb]:
+  https://www.blurb.com/b/9621951-category-theory-for-programmers-new-edition-hardco
+[buy scala edition on blurb]:
+  https://www.blurb.com/b/9603882-category-theory-for-programmers-scala-edition-pape
+[v1.3.0 github release link]:
+  https://github.com/hmemcpy/milewski-ctfp-pdf/releases/tag/v1.3.0
+[nix website]: https://nixos.org/nix/
+[nix download website]: https://nixos.org/download.html
--- flake.nix
+++ flake.nix
@@ -1,149 +1,203 @@
 {
   description = "Category Theory for Programmers";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system: let
-    inherit (nixpkgs) lib;
-
-    pkgs = nixpkgs.legacyPackages.${system};
-
-    ###########################################################################
-    # LaTeX Environment
-    texliveEnv = pkgs.texlive.combine {
-      inherit (pkgs.texlive)
-        bookcover
-        textpos
-        fgruler
-        tcolorbox
-        fvextra
-        framed
-        newtx
-        nowidow
-        emptypage
-        wrapfig
-        subfigure
-        adjustbox
-        collectbox
-        tikz-cd
-        imakeidx
-        idxlayout
-        titlesec
-        subfiles
-        lettrine
-        upquote
-        libertine
-        mweights
-        fontaxes
-        mdframed
-        needspace
-        xifthen
-        ifnextok
-        currfile
-        noindentafter
-        ifmtarg
-        scheme-medium
-        listings
-        minted
-        microtype
-        babel
-        todonotes
-        chngcntr
-        ifplatform
-        xstring
-        minifp
-        titlecaps
-        enumitem
-        environ
-        trimspaces
-        l3packages
-        zref
-        catchfile
-        import
-        ;
-    };
-
-    ###########################################################################
-    # Python Environment
-
-    # Pin the Python version and its associated package set in a single place.
-    python = pkgs.python38;
-    pythonPkgs = pkgs.python38Packages;
-
-    pygments-style-github = pythonPkgs.buildPythonPackage rec {
-      pname = "pygments-style-github";
-      version = "0.4";
-
-      doCheck = false; # Hopefully someone else has run the tests.
-
-      src = pythonPkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "19zl8l5fyb8z3j8pdlm0zbgag669af66f8gn81ichm3x2hivvjhg";
-      };
-
-      # Anything depending on this derivation is probably also gonna want
-      # pygments to be available.
-      propagatedBuildInputs = with pythonPkgs; [ pygments ];
-    };
-
-    pythonEnv = python.withPackages (p: [ p.pygments pygments-style-github ]);
-
-    commonAttrs = {
-      nativeBuildInputs = [ texliveEnv pythonEnv pkgs.which ];
-      FONTCONFIG_FILE = pkgs.makeFontsConf {
-        fontDirectories = with pkgs; [ inconsolata-lgc libertine libertinus ];
-      };
-    };
-
-    mkLatex = variant: edition: let
-      maybeVariant = lib.optionalString (variant != null) "-${variant}";
-      maybeEdition = lib.optionalString (edition != null) "-${edition}";
-      variantStr = if variant == null then "reader" else variant;
-      suffix = maybeVariant + maybeEdition;
-      basename = "ctfp-${variantStr}${maybeEdition}";
-      version = self.shortRev or self.lastModifiedDate;
-    in pkgs.stdenv.mkDerivation (commonAttrs // {
-      inherit basename version;
-
-      name = "ctfp${suffix}-${version}";
-      fullname = "ctfp${suffix}";
-      src = "${self}/src";
-
-      configurePhase = ''
-        echo -n "\\newcommand{\\OPTversion}{$version}" > version.tex
-      '';
-
-      buildPhase = ''
-        latexmk -shell-escape -interaction=nonstopmode -halt-on-error \
-          -norc -jobname=ctfp -pdflatex="xelatex %O %S" -pdf "$basename.tex"
-      '';
-
-      installPhase = "install -m 0644 -vD ctfp.pdf \"$out/$fullname.pdf\"";
-
-      passthru.packageName = "ctfp${suffix}";
-    });
-
-    editions = [ null "scala" "ocaml" "reason" ];
-    variants = [ null "print" ];
-  in {
-    # nix build .#ctfp
-    # nix build .#ctfp-print
-    # nix build .#ctfp-print-ocaml
-    # etc etc
-    packages = lib.listToAttrs (lib.concatMap (variant: map (edition: rec {
-      name = value.packageName;
-      value = mkLatex variant edition;
-    }) editions) variants);
-
-    # nix build .
-    defaultPackage = self.packages.${system}.ctfp;
-
-    # nix develop .
-    devShell = pkgs.mkShell (commonAttrs // {
-      nativeBuildInputs = commonAttrs.nativeBuildInputs ++ [
-        pkgs.git pkgs.gnumake
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
-    });
-  });
+
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        ...
+      }: let
+        inherit (nixpkgs) lib;
+
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        ###########################################################################
+        # LaTeX font
+        inconsolata-lgc-latex = pkgs.stdenvNoCC.mkDerivation {
+          name = "inconsolata-lgc-latex";
+          pname = "inconsolata-lgc-latex";
+
+          src = pkgs.inconsolata-lgc;
+
+          dontConfigure = true;
+          sourceRoot = ".";
+
+          installPhase = ''
+            runHook preInstall
+
+            find $src -name '*.ttf' -exec install -m644 -Dt $out/fonts/truetype/public/inconsolata-lgc/ {} \;
+            find $src -name '*.otf' -exec install -m644 -Dt $out/fonts/opentype/public/inconsolata-lgc/ {} \;
+
+            runHook postInstall
+          '';
+
+          tlType = "run";
+        };
+
+        ###########################################################################
+        # LaTeX Environment
+        texliveEnv = pkgs.texlive.combine {
+          inherit
+            (pkgs.texlive)
+            adjustbox
+            alegreya
+            babel
+            bookcover
+            catchfile
+            chngcntr
+            collectbox
+            currfile
+            emptypage
+            enumitem
+            environ
+            fgruler
+            fontaxes
+            framed
+            fvextra
+            idxlayout
+            ifmtarg
+            ifnextok
+            ifplatform
+            imakeidx
+            import
+            inconsolata
+            l3packages
+            lettrine
+            libertine
+            libertinus-fonts
+            listings
+            mdframed
+            microtype
+            minifp
+            minted
+            mweights
+            needspace
+            newtx
+            noindentafter
+            nowidow
+            scheme-medium
+            subfigure
+            subfiles
+            textpos
+            tcolorbox
+            tikz-cd
+            titlecaps
+            titlesec
+            todonotes
+            trimspaces
+            upquote
+            wrapfig
+            xifthen
+            xpatch
+            xstring
+            zref
+            ;
+
+          inconsolata-lgc-latex = {
+            pkgs = [inconsolata-lgc-latex];
+          };
+        };
+
+        ###########################################################################
+        # Python Environment
+
+        # Pin the Python version and its associated package set in a single place.
+        python = pkgs.python311;
+        pythonPkgs = pkgs.python311Packages;
+
+        pygments-style-github = pythonPkgs.buildPythonPackage rec {
+          pname = "pygments-style-github";
+          version = "0.4";
+
+          doCheck = false; # Hopefully someone else has run the tests.
+
+          src = pythonPkgs.fetchPypi {
+            inherit pname version;
+            sha256 = "19zl8l5fyb8z3j8pdlm0zbgag669af66f8gn81ichm3x2hivvjhg";
+          };
+
+          # Anything depending on this derivation is probably also gonna want
+          # pygments to be available.
+          propagatedBuildInputs = with pythonPkgs; [pygments];
+        };
+
+        pythonEnv = python.withPackages (p: [p.pygments pygments-style-github]);
+
+        commonAttrs = {
+          nativeBuildInputs = [texliveEnv pythonEnv pkgs.which];
+        };
+
+        mkLatex = variant: edition: let
+          maybeVariant = lib.optionalString (variant != null) "-${variant}";
+          maybeEdition = lib.optionalString (edition != null) "-${edition}";
+          variantStr =
+            if variant == null
+            then "reader"
+            else variant;
+          suffix = maybeVariant + maybeEdition;
+          basename = "ctfp-${variantStr}${maybeEdition}";
+          version = self.shortRev or self.lastModifiedDate;
+        in
+          pkgs.stdenv.mkDerivation (commonAttrs
+            // {
+              inherit basename version;
+
+              name = "ctfp${suffix}";
+              fullname = "ctfp${suffix}";
+              src = "${self}/src";
+
+              configurePhase = ''
+                substituteInPlace "version.tex" --replace "dev" "${version}"
+              '';
+
+              buildPhase = ''
+                latexmk -file-line-error -shell-escape -logfilewarninglist -interaction=nonstopmode -halt-on-error \
+                  -norc -jobname=ctfp -pdflatex="xelatex %O %S" -pdfxe "$basename.tex"
+              '';
+
+              installPhase = "install -m 0644 -vD ctfp.pdf \"$out/$fullname.pdf\"";
+
+              passthru.packageName = "ctfp${suffix}";
+            });
+
+        editions = [null "scala" "ocaml" "reason"];
+        variants = [null "print"];
+      in rec {
+        formatter = pkgs.alejandra;
+
+        packages = lib.listToAttrs (lib.concatMap (variant:
+          map (edition: rec {
+            name = value.packageName;
+            value = mkLatex variant edition;
+          })
+          editions)
+        variants);
+
+        # nix develop .
+        devShells.default = pkgs.mkShellNoCC (commonAttrs
+          // {
+            nativeBuildInputs =
+              commonAttrs.nativeBuildInputs
+              ++ [
+                pkgs.git
+                pkgs.gnumake
+              ];
+          });
+      };
+    };
 }
