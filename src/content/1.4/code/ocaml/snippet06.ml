b29fc3a6cdee34d0a244dc43caca5e38f95f4e4d
ComanderP <8596680+ComanderP@users.noreply.github.com>
2025-01-03 18:14:19 +0100

    Update Ch. 4 OCaml snippets

--- src/content/1.4/code/ocaml/snippet06.ml
+++ src/content/1.4/code/ocaml/snippet06.ml
@@ -1,3 +1,5 @@
-let to_words : string -> string list writer =
- fun s -> String.split s ~on:' ', "to_words "
-;;
+let up_case (s : string) : string writer =
+  (String.uppercase_ascii s, "up_case ")
+
+let to_words (s : string) : string list writer =
+  (String.split_on_char ' ' s, "to_words ")
