56f05f08241d648a8a832b6d1f09a5b0d10fa379
ComanderP <8596680+ComanderP@users.noreply.github.com>
2025-01-03 18:22:52 +0100

    Update Ch. 20 OCaml snippets

--- src/content/3.4/code/ocaml/snippet20.ml
+++ src/content/3.4/code/ocaml/snippet20.ml
@@ -1,5 +1,8 @@
 module Process_Bind_Without_Do
-    (W : Monad_Bind with type 'a m = (string, 'a) writer) =
+    (W : Monad_Bind with type 'a t = (string, 'a) writer) =
 struct
-  let process s = W.(up_case s >>= fun up_str -> to_words up_str)
+  open W
+
+  (* let* x = m in n is equivalent to m >>= fun x -> n *)
+  let process s = up_case s >>= fun up_str -> to_words up_str
 end
