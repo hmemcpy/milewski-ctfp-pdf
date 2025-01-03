ba65eed5e297de9e395dda303b89d0a0122e2eba
ComanderP <8596680+ComanderP@users.noreply.github.com>
2025-01-03 18:22:33 +0100

    Update Ch. 19 OCaml snippets

--- src/content/3.3/code/ocaml/snippet01.ml
+++ src/content/3.3/code/ocaml/snippet01.ml
@@ -1,1 +1,4 @@
+(* Note: in OCaml, strings are not equivalent to char lists.
+   Strings are treated as immutable sequences of bytes, while
+   a char list is a linked list of characters. *)
 type string = char list
