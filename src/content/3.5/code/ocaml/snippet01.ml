16d42e6e04eb4860d683f880188ba67d992073df
ComanderP <8596680+ComanderP@users.noreply.github.com>
2025-01-03 18:23:15 +0100

    Update Ch. 21 OCaml snippets

--- src/content/3.5/code/ocaml/snippet01.ml
+++ src/content/3.5/code/ocaml/snippet01.ml
@@ -1,6 +1,9 @@
-module List_Monad : Monad_Join = struct
-  type 'a m = 'a list
+(* For infinite lists, like in Haskell, we use the [Seq] module. *)
+module Seq_Monad : Monad_Join with type 'a t = 'a Seq.t = struct
+  type 'a t = 'a Seq.t
 
-  let join = List.concat
-  let return a = [ a ]
+  let join = Seq.concat
+
+  (* [Seq] already has a [return] function. *)
+  let return a = Seq.return a
 end
--- src/content/3.5/code/ocaml/snippet12.ml
+++ src/content/3.5/code/ocaml/snippet12.ml
@@ -1,10 +1,9 @@
 module ReaderMonad (T : sig
   type t
-end) : Monad_Bind = struct
-  type 'a m = (T.t, 'a) reader
-
-  let return a = Reader (fun e -> a)
+end) : Monad_Bind with type 'a t = (T.t, 'a) reader = struct
+  type 'a t = (T.t, 'a) reader
 
   let ( >>= ) ra k =
     Reader (fun e -> run_reader (k (run_reader ra e)) e)
+  let return a = Reader (fun e -> a)
 end
