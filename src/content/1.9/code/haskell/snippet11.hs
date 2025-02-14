a5fbb28640ecf7e578b8b7749a5d5f7836071eea
Masahiro Kitagawa <arctica0316@gmail.com>
2025-02-14 16:58:12 +0900

    style: Wrap long line of code

--- src/content/1.9/code/haskell/snippet11.hs
+++ src/content/1.9/code/haskell/snippet11.hs
@@ -1,2 +1,8 @@
-f (Left n) = if n < 0 then "Negative int" else "Positive int"
-f (Right x) = if x < 0.0 then "Negative double" else "Positive double"
+f (Left n) =
+  if n < 0
+    then "Negative int"
+    else "Positive int"
+f (Right x) =
+  if x < 0.0
+    then "Negative double"
+    else "Positive double"
